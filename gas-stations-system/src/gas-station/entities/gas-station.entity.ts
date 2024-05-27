3505db70528fbaec6686b9bc9814ac976411bc8d
Вікторія Дерпак <vikusya.derpak@gmail.com>
2024-05-27 12:02:01 +0300

    Described GasStation Module

--- gas-stations-system/src/gas-station/entities/gas-station.entity.ts
+++ gas-stations-system/src/gas-station/entities/gas-station.entity.ts
@@ -1,1 +1,10 @@
-export class GasStation {}
+{}import {Column, Entity, PrimaryGeneratedColumn} from 'typeorm'
+
+@Entity()
+export class GasStation{
+    @PrimaryGeneratedColumn()
+    StationID: number;
+
+    @Column({type:'varchar'})
+    Address: string;
+}
--- gas-stations-system/src/gas-station/gas-station.service.ts
+++ gas-stations-system/src/gas-station/gas-station.service.ts
@@ -1,26 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { CreateGasStationDto } from './dto/create-gas-station.dto';
 import { UpdateGasStationDto } from './dto/update-gas-station.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { GasStation } from './entities/gas-station.entity';
 
 @Injectable()
 export class GasStationService {
-  create(createGasStationDto: CreateGasStationDto) {
-    return 'This action adds a new gasStation';
+
+  constructor(
+    @InjectRepository(GasStation) private readonly gasStationRepository: Repository<GasStation>,
+  ){}
+
+  create(createGasStationDto: CreateGasStationDto): Promise<GasStation> {
+    const gasStation: GasStation = new GasStation();
+    gasStation.Address = createGasStationDto.Address;
+    return this.gasStationRepository.save(gasStation);
   }
 
-  findAll() {
-    return `This action returns all gasStation`;
+  findAll(): Promise<GasStation[]> {
+    return this.gasStationRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} gasStation`;
+  findOne(StationID: number): Promise<GasStation> {
+    return this.gasStationRepository.findOneBy({StationID});
   }
 
-  update(id: number, updateGasStationDto: UpdateGasStationDto) {
-    return `This action updates a #${id} gasStation`;
+  update(StationID: number, updateGasStationDto: UpdateGasStationDto): Promise<GasStation> {
+    const gasStation: GasStation = new GasStation();
+    gasStation.Address=updateGasStationDto.Address;
+    gasStation.StationID = StationID;
+    return this.gasStationRepository.save(gasStation);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} gasStation`;
+  remove(StationID: number): Promise<{ affected?: number }> {
+    return this.gasStationRepository.delete(StationID);
   }
 }
