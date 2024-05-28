442b4c55d07a5a424303e8e89f87067d787880b9
Вікторія Дерпак <vikusya.derpak@gmail.com>
2024-05-28 14:05:46 +0300

    Added Fuel-In-Stock model

--- gas-stations-system/src/fuel-in-stock/dto/create-fuel-in-stock.dto.ts
+++ gas-stations-system/src/fuel-in-stock/dto/create-fuel-in-stock.dto.ts
@@ -1,1 +1,16 @@
-export class CreateFuelInStockDto {}
+import { IsDecimal, IsInt, IsNegative, IsNotEmpty, Min } from "class-validator";
+
+export class CreateFuelInStockDto {
+    @IsInt()
+    @IsNotEmpty()
+    StationID: number;
+
+    @IsInt()
+    @IsNotEmpty()
+    FuelID: number;
+
+    @IsDecimal()
+    @IsNotEmpty()
+    @Min(0)
+    Quantity: number;
+}
--- gas-stations-system/src/fuel-in-stock/fuel-in-stock.service.ts
+++ gas-stations-system/src/fuel-in-stock/fuel-in-stock.service.ts
@@ -1,26 +1,74 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateFuelInStockDto } from './dto/create-fuel-in-stock.dto';
 import { UpdateFuelInStockDto } from './dto/update-fuel-in-stock.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FuelInStock } from './entities/fuel-in-stock.entity';
+import { Repository } from 'typeorm';
+import { Fuel } from 'src/fuel/entities/fuel.entity';
+import { GasStation } from 'src/gas-station/entities/gas-station.entity';
 
 @Injectable()
 export class FuelInStockService {
-  create(createFuelInStockDto: CreateFuelInStockDto) {
-    return 'This action adds a new fuelInStock';
+
+  constructor(
+    @InjectRepository(FuelInStock)
+    private readonly fuelInStockRepository: Repository<FuelInStock>,
+
+    @InjectRepository(Fuel)
+    private readonly fuelRepository: Repository<Fuel>,
+
+    @InjectRepository(GasStation)
+    private readonly gasStationRepository: Repository<GasStation>
+  ){}
+
+  async create(createFuelInStockDto: CreateFuelInStockDto) : Promise<FuelInStock> {
+    const { StationID, FuelID, Quantity } = createFuelInStockDto;
+    
+    const gasStation = await this.gasStationRepository.findOneBy({StationID});
+    if(!gasStation)
+      throw new NotFoundException(`There is no station with ${StationID}`);
+    
+    const fuel = await this.fuelRepository.findOneBy({FuelID});
+    if(!fuel)
+      throw new NotFoundException(`There is no fuel with ${FuelID}`);
+
+    const fuelInStock = new FuelInStock();
+    fuelInStock.Quantity = Quantity;
+    fuelInStock.Station = gasStation;
+    fuelInStock.Fuel = fuel;
+
+    return this.fuelInStockRepository.save(fuelInStock);
   }
 
-  findAll() {
-    return `This action returns all fuelInStock`;
+  findAll() : Promise<FuelInStock[]> {
+    return this.fuelInStockRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} fuelInStock`;
+  findOne(StockID: number) : Promise<FuelInStock> {
+    return this.fuelInStockRepository.findOneBy({StockID});
   }
 
-  update(id: number, updateFuelInStockDto: UpdateFuelInStockDto) {
-    return `This action updates a #${id} fuelInStock`;
+  async update(StockID: number, updateFuelInStockDto: UpdateFuelInStockDto) : Promise<FuelInStock> {
+    const { StationID, FuelID, Quantity } = updateFuelInStockDto;
+    
+    const gasStation = await this.gasStationRepository.findOneBy({StationID});
+    if(!gasStation)
+      throw new NotFoundException(`There is no station with ${StationID}`);
+    
+    const fuel = await this.fuelRepository.findOneBy({FuelID});
+    if(!fuel)
+      throw new NotFoundException(`There is no fuel with ${FuelID}`);
+
+    const fuelInStock = new FuelInStock();
+    fuelInStock.Quantity = Quantity;
+    fuelInStock.Station = gasStation;
+    fuelInStock.Fuel = fuel;
+    fuelInStock.StockID = StockID;
+
+    return this.fuelInStockRepository.save(fuelInStock);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} fuelInStock`;
+  remove(StockID: number) : Promise<{affected?: number}> {
+    return this.fuelInStockRepository.delete(StockID);
   }
 }
